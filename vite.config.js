69fb12e0d79d6f6a5341aff33e06d29f69c44d55
Tc001 <tc001@t0.lv>
2023-07-04 15:30:39 +0300

    Add lightningcss

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,22 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-// import devtools from 'solid-devtools/vite';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import lightningcss from "vite-plugin-lightningcss";
 
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    transformMode: {
-      web: [/\.jsx?$/],
-    },
-    setupFiles: ['node_modules/@testing-library/jest-dom/extend-expect.js'],
-    // otherwise, solid would be loaded twice:
-    deps: { registerNodeLoader: true },
-    // if you have few tests, try commenting one
-    // or both out to improve performance:
-    // threads: false,
-    // isolate: false,
-  },
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-  ],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
-  resolve: {
-    conditions: ['development', 'browser'],
-  },
+	plugins: [
+		solidPlugin(),
+		lightningcss({
+			browserslist: ">= 0.25%",
+		}),
+	],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: "esnext",
+		sourcemap: true,
+	},
+	resolve: {
+		conditions: ["development", "browser"],
+	},
 });
